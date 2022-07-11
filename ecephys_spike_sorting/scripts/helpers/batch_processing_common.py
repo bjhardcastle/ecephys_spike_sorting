ec3239ead83a56f76f9441146f837053d87b9848
Ben <ben.hardcastle@alleninstitute.org>
2022-07-11 14:17:10 -0700

    Copy more small events files from raw v0.6.0 data

--- ecephys_spike_sorting/scripts/helpers/batch_processing_common.py
+++ ecephys_spike_sorting/scripts/helpers/batch_processing_common.py
@@ -343,6 +343,24 @@ class processing_session():
                 src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-LFP\TTL\timestamps.npy"
                 dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.1\TTL_1\event_timestamps.npy"
                 move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-AP\TTL\sample_numbers.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.0\TTL_1\sample_numbers.npy"
+                move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-AP\TTL\full_words.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.0\TTL_1\full_words.npy"
+                move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-LFP\TTL\states.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.1\TTL_1\channel_states.npy"
+                move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-LFP\TTL\timestamps.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.1\TTL_1\event_timestamps.npy"
+                move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-LFP\TTL\sample_numbers.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.1\TTL_1\sample_numbers.npy"
+                move(src,dest)
+                src= fR"{rec_root}\events\Neuropix-PXI-100.Probe{probe}-LFP\TTL\full_words.npy"
+                dest= fR"{dest_dir}_probe{probe}_sorted\events\Neuropix-PXI-100.1\TTL_1\full_words.npy"
+                move(src,dest)
                 finished = True
             except Exception as e:
                 print(e)
